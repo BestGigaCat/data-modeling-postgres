2f3c65cc271556c7bbf722e351d4948714614f96
BestGigaCat <bestgigacatna@gmail.com>
2019-07-07 18:07:27 -0700

    add queries

--- sql_queries.py
+++ sql_queries.py
@@ -67,25 +67,44 @@ time_table_create = ("""
 # INSERT RECORDS
 
 songplay_table_insert = ("""
-
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
 """)
 
 user_table_insert = ("""
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id) DO NOTHING;
 """)
 
 song_table_insert = ("""
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""
+INSERT INTO artists (artist_id, name, location, lattitude, longitude)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id) DO NOTHING;
 """)
 
 
 time_table_insert = ("""
+INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+VALUES (%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (start_time) DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
+    SELECT s.song_id, a.artist_id
+    FROM songs s, users u
+    WHERE s.artist_id = u.artist_id
+    AND a.name = (%s) 
+    AND s.title = (%s) 
+    AND s.duration = (%s);
 """)
 
 # QUERY LISTS
